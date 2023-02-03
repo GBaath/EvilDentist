7941d4a9b449285b213dae4aabf723c67219c460
GVBååth <gustav.baath02@gmail.com>
2023-02-03 22:59:26 +0100

    teeth

--- Assets/Script/MouseFollow.cs
+++ Assets/Script/MouseFollow.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class MouseFollow : MonoBehaviour
 {
     [SerializeField] float moveSens = 5;
+    [SerializeField] SpriteRenderer hands;
     void Update()
     {
         transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSens*Time.deltaTime);
+        if (Camera.main.WorldToScreenPoint(transform.position).x > Screen.width / 2)
+        {
+            hands.flipX = true;
+        }
+        else
+            hands.flipX = false;
     }
 }
