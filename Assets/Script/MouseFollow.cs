a2b1ec384a329f6e42e36049ba7ddb714b1b9a0a
GVBååth <gustav.baath02@gmail.com>
2023-02-04 10:36:05 +0100

    added sway

--- Assets/Script/MouseFollow.cs
+++ Assets/Script/MouseFollow.cs
@@ -6,14 +6,47 @@ public class MouseFollow : MonoBehaviour
 {
     [SerializeField] float moveSens = 5;
     [SerializeField] SpriteRenderer hands;
+
+    [SerializeField]bool preciscionMove;
+
+    [SerializeField]float swayScale = 1;
+    float ogSwayScale;
+
+    private void Start()
+    {
+        ogSwayScale = swayScale;
+    }
+
     void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSens*Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space)||Input.GetKeyUp(KeyCode.Space))
+            TogglePreciscionMove();
+
         if (Camera.main.WorldToScreenPoint(transform.position).x > Screen.width / 2)
         {
             hands.flipX = true;
         }
         else
             hands.flipX = false;
+
+        //sway
+        transform.position += (Vector3)Random.insideUnitCircle.normalized*Time.deltaTime*swayScale;
+        
+        transform.position = Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSens * Time.deltaTime); //Vector3.Lerp(transform.position,transform.position+move,swaySpeed*Time.deltaTime);
+
+
+    }
+    public void TogglePreciscionMove()
+    {
+        if (swayScale == 1)
+        {
+            swayScale = ogSwayScale;
+            moveSens *= 3;
+        }
+        else
+        {
+            swayScale = 1;
+            moveSens /= 3;
+        }
     }
 }
